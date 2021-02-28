fd33a4c741e76a866eb86acda94d415485034a27
rockuul <rockuul888@gmail.com>
2021-02-28 18:11:22 +0300

    Add 2 objects, 2 materials and script rotator

---
+++ Assets/Scripts/Roatator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roatator : MonoBehaviour
+{
+    public float speed = 0f;
+
+    public bool ForwardX = false;
+    public bool ForwardY = false;
+    public bool ForwardZ = false;
+
+    void Update()
+    {
+        if (ForwardX == true)
+        {
+            transform.Rotate(Time.deltaTime * speed, 0, 0, Space.Self);
+        }
+        if (ForwardY == true)
+        {
+            transform.Rotate(0, Time.deltaTime * speed, 0, Space.Self);
+        }
+        if (ForwardZ == true)
+        {
+            transform.Rotate(0, 0, Time.deltaTime * speed, Space.Self);
+        }
+    }
+}
