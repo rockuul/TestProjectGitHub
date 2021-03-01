a614cb3398afb8471d59b2fb5012e4561a8ac0ee
rockuul <rockuul888@gmail.com>
2021-03-01 15:22:48 +0300

    Change rotator script

--- Assets/Scripts/Roatator.cs
+++ Assets/Scripts/Roatator.cs
@@ -6,23 +6,24 @@ public class Roatator : MonoBehaviour
 {
     public float speed = 0f;
 
-    public bool ForwardX = false;
-    public bool ForwardY = false;
-    public bool ForwardZ = false;
+    //public bool ForwardX = false;
+    //public bool ForwardY = false;
+    //public bool ForwardZ = false;
 
     void Update()
     {
-        if (ForwardX == true)
-        {
-            transform.Rotate(Time.deltaTime * speed, 0, 0, Space.Self);
-        }
-        if (ForwardY == true)
-        {
-            transform.Rotate(0, Time.deltaTime * speed, 0, Space.Self);
-        }
-        if (ForwardZ == true)
-        {
-            transform.Rotate(0, 0, Time.deltaTime * speed, Space.Self);
-        }
+        transform.Rotate(0, Time.deltaTime * speed, 0, Space.Self);
+        //if (ForwardX == true)
+        //{
+        //    transform.Rotate(Time.deltaTime * speed, 0, 0, Space.Self);
+        //}
+        //if (ForwardY == true)
+        //{
+        //    transform.Rotate(0, Time.deltaTime * speed, 0, Space.Self);
+        //}
+        //if (ForwardZ == true)
+        //{
+        //    transform.Rotate(0, 0, Time.deltaTime * speed, Space.Self);
+        //}
     }
 }
